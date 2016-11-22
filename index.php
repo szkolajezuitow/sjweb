34d2f66a9c77359052463a6dab95dcc2142e9f8d
kamilsj <kboberek@gmail.com>
2016-11-22 14:52:54 +0100

    First commit
    just a singe commit of the newest website

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta author="Kamil Boberek">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		
+		<title>Szkoła jezuitów - strona wyboru</title>
+		
+		<link rel="stylesheet" type="text/css" href="css/index.css" >
+		<script src="js/jquery.js" type="text/javascript"></script>
+		
+
+				
+
+	</head>
+	
+	<body>
+		<div class="upcolor">
+			<div id="name">
+				<div style="padding-top: 15px; padding-left: 150px; font-size: 40px; color: white; text-align: center;">
+					<b>SZKOŁA JEZUITÓW</b>
+				</div>
+			</div> 
+			<img id="logoPosition" src="assets/logo.png" />
+			
+		</div>
+
+		<div id="showmiddle">
+			<div id="box1" class="box">
+				<div style="margin-top: 50px;">Szkoła<br>
+				Podstawowa</div>
+			</div>
+			
+	
+			<div id="box2" class="box">
+				<div style="margin-top: 50px;">Liceum<br>
+				Ogólnokształcące</div>
+			</div>
+		</div>
+
+	</body>
+	<script src="js/transit.js" type="text/javascript"></script>
+	<script src="js/main.js" type="text/javascript"></script>
+</html>
---
+++ js/main.js
@@ -0,0 +1,44 @@
+
+
+$("#box1").click(function(){
+	
+	window.location.href= "http://zsj.jointheway.com/podstawowa/";
+	
+});
+
+$("#box1").hover(function(){
+	
+	$(this).transition({
+		scale: 0.75
+	})},
+	
+	function(){
+	
+		$(this).transition({
+		scale: 1
+		})
+	
+		
+	});
+
+
+$("#box2").hover(function(){
+	
+	$(this).transition({
+		scale: 0.75
+	})},
+	
+	function(){
+	
+		$(this).transition({
+		scale: 1
+		})
+	
+		
+	});
+
+$("#box2").click(function(){
+	
+	window.location.href = "http://jezuici.edu.pl";
+	
+});
